5dbc34daf1e4763793ec33475562ad64d52becee
W0w <58712352+zhaosiyuan1098@users.noreply.github.com>
2024-01-30 00:37:35 +0800

    finish 121

--- python/121.best-time-to-buy-and-sell-stock.py
+++ python/121.best-time-to-buy-and-sell-stock.py
@@ -17,21 +17,21 @@ from typing import *
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        inf = int(1e9)
-        minprice = inf
-        maxprofit = 0
-        for price in prices:
-            maxprofit = max(price - minprice, maxprofit)
-            minprice = min(price, minprice)
-        return maxprofit
-        # maxdiff=[[None]*len(prices) for i in range(len(prices))]
-        # for i in range(len(prices)):
-        #     maxdiff[i][i]=0
-        # for diff in range(1,len(prices)):
-        #     for j in range(len(prices)-diff):
-        #         maxdiff[j][j+diff]=max(maxdiff[j+1][j+diff],maxdiff[j][j+diff-1],prices[j+diff]-prices[j])
-        # # print(maxdiff)
-        # return maxdiff[0][-1]      
+        maxdiff=[[None]*len(prices) for i in range(len(prices))]
+        for i in range(len(prices)):
+            maxdiff[i][i]=0
+        for diff in range(1,len(prices)):
+            for j in range(len(prices)-diff):
+                maxdiff[j][j+diff]=max(maxdiff[j+1][j+diff],maxdiff[j][j+diff-1],prices[j+diff]-prices[j])
+        # print(maxdiff)
+        return maxdiff[0][-1]  
+        # inf = int(1e9)
+        # minprice = inf
+        # maxprofit = 0
+        # for price in prices:
+        #     maxprofit = max(price - minprice, maxprofit)
+        #     minprice = min(price, minprice)
+        # return maxprofit    
 # @lc code=end
 
 
